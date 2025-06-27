6beb148116b9b03cde7050669fb98890421ef293
Om Rabadia <orrabadia@gmail.com>
2025-06-27 15:18:22 -0700

    Connect Server to Frontend

--- frontend/src/hooks/useCart.js
+++ frontend/src/hooks/useCart.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { sample_items } from '../../../backend/src/data';
+
 
 const CartContext = createContext(null);
 
--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,15 +1,28 @@
-import { sample_items, sample_tags } from "../../../backend/src/data";
+import axios from "axios";
 
-export const getAll = async () => sample_items;
 
-export const search = async (searchTerm) => sample_items.filter(item => 
-    item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+export const getAll = async () => {
+    const { data } = await axios.get('/api/items/menu');
+    return data;
+}
 
-export const getAllTags = async () => sample_tags;
+export const search = async (searchTerm) => {
+    const { data } = await axios.get('/api/items/menu/search/' + searchTerm)
+    return data;
+}
+
+export const getAllTags = async () => {
+    const { data } = await axios.get('/api/items/menu/tags')
+    return data;
+}
 
 export const getAllByTag = async tag => {
     if (tag === 'All') return getAll();
-    return sample_items.filter(item => item.tags?.includes(tag));
+    const { data } = await axios.get('/api/items/menu/tag/' + tag)
+    return data; 
 };
 
-export const getByID = async itemID => sample_items.find(item => item.id === itemID);
+export const getByID = async itemID => {
+    const { data } = await axios.get('/api/items/menu/item/' + itemID)
+    return data; 
+}
